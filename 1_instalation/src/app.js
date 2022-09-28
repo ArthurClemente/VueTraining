2b15496458ddc76faf106f85c9ebb21a98ed84b4
Arthur Clemente <arthurclementte@hotmail.com>
2022-09-28 09:56:26 -0300

    Input and data binding. A quick lesson on how data binding works on Vue.

--- 1_instalation/src/app.js
+++ 1_instalation/src/app.js
@@ -3,8 +3,18 @@ const MyNameApp = {
    // data() return an object with the data that will be used in the app
    data() {
       return{
-         name: "Johnny Joestar",
-         idade: 19,
+         name: "",
+         age: 19,
+         input_name: "",
+      }
+   },
+   methods: {
+      submitForm(e) {
+         // Prevent the form from submitting
+         e.preventDefault();
+
+         // The name is seted with the submit event
+         this.name = this.input_name;
       }
    }
 }
