741947ded842a1342b97cf9d3cbbed9f2df6c2db
Arthur Clemente <arthurclementte@hotmail.com>
2022-10-11 08:59:53 -0300

    Example about dynamic attributes with v-bind.

--- 2_vue_project/src/components/Info.vue
+++ 2_vue_project/src/components/Info.vue
@@ -3,6 +3,7 @@
       <!-- This elements will be displayed when the directive within v-if is true -->
       <!-- Everything in the quotes will be executed as JavaScript -->
       <div v-if="working">
+         <Picture />
          <p>he works for Marcellus Wallace</p>
          <ul>
             <li>he had a brother, but he was taken down by a cop</li>
@@ -11,6 +12,8 @@
          </ul>
          <!-- v-show works similar to v-if/else, but only to render elements -->
          <p v-show="call">His number is {{ number }}</p>
+         <!-- v-bind link an html attribute to a vueJs data -->
+         <p>If you want to know more about me, visit <a v-bind:href="myLink" target="_blank">this page</a></p>
       </div>
       <!-- Otherwise this will be displayed -->
       <p v-else>he's dead</p>
@@ -19,13 +22,19 @@
 </template>
 
 <script>
+import Picture from './Picture.vue';
+
    export default{
       name: 'Info',
+      components: {
+         Picture
+      },
       data() {
          return {
             working: true,
             call: false,
-            number: '555-555-5555'
+            number: '555-555-5555',
+            myLink: 'https://pulpfiction.fandom.com/wiki/Vincent_Vega'
          }
       }
    }
---
+++ 2_vue_project/src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+   <!-- : is equivalent to v-bind: -->
+   <img :src="avatar" :alt="description">
+</template>
+<script>
+   export default {
+      name: 'Picture',
+      data() {
+         return {
+            avatar: "/img/vega.jfif",
+            description: "Vincent Vega"
+         }
+      }
+
+   }
+</script>
