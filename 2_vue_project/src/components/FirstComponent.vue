0ea18e908a8eaa89af9b628eb559ebac04bfa217
Arthur Clemente <arthurclementte@hotmail.com>
2022-10-24 10:07:55 -0300

    Example of how multiple events works in vue.

--- 2_vue_project/src/components/FirstComponent.vue
+++ 2_vue_project/src/components/FirstComponent.vue
@@ -3,19 +3,25 @@
       <!-- Vue components can only have one root element-->
       <h1>Hello VUE!!</h1>
       <p>My name is {{ name }}, I'm a {{ profession }}</p> <!-- {{}} "execute" vue code -->
+      <MultipleEvents />
    </div>
 </template>
 
 <script>
-export default {
-   // Exported object
-   name: 'FirstComponent',
-   // Data to be used in the component
-   data() {
-      return {
-         name: "Diego B.",
-         profession: "Jockey",
+   // Imported component that will be used in the template
+   import MultipleEvents from './MultipleEvents.vue'
+   export default {
+      // Exported object
+      name: 'FirstComponent',
+      components: {
+         MultipleEvents
+      },
+      // Data to be used in the component
+      data() {
+         return {
+            name: "Diego B.",
+            profession: "Jockey",
+         }
       }
    }
-}
 </script>
---
+++ 2_vue_project/src/components/MultipleEvents.vue
@@ -0,0 +1,34 @@
+<template>
+   <div> <!-- All events will be called in sequence when the button clicked -->
+      <button @click="first(txt1, $event), second(txt2, $event), third()">Run multiple events</button>
+      <p>{{ multiple_events }}</p>
+   </div>
+</template>
+<script>
+   export default {
+      name: 'MultipleEvents',
+      data() {
+         return {
+            multiple_events: "",
+            txt1: "First event activated",
+            txt2: "Second event activated"
+         }
+      },
+      methods: {
+         first(txt, e) {
+            this.multiple_events= txt;
+         },
+         second(txt, e) {
+            setTimeout(() => this.multiple_events = txt, 500);
+         },
+         third() {
+            setTimeout(() => this.multiple_events = "", 1500);
+         }
+      }
+   }
+</script>
+<style scoped>
+   div {
+      margin: 100px;
+   }
+</style>
